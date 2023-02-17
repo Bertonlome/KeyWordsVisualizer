7c219b2cc7178957114cb0421d5755b9e24c683a
Florian TURLEPIN <florian.turlepin@etu.estia.fr>
2023-02-17 11:41:00 +0100

    test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KeyWordsVisualizer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,17 +19,15 @@ namespace KeyWordsVisualizer
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
             DataAccess myDataAccess = new DataAccess();
             myDataAccess.createDbFile();
-            this.InitializeComponent();
-            //Output.ItemsSource = DataAccess.GetCollabList();
+            Output.ItemsSource = DataAccess.GetCollabList();
         }
-
         private void AddCollab(object sender, RoutedEventArgs e)
         {
             Collab myCollab = new Collab
@@ -39,13 +38,69 @@ namespace KeyWordsVisualizer
 
             string[] skillName = collabSkillInput.Text.Split(',');
 
-            //DataAccess.AddCollab(myCollab, skillName);
+            DataAccess.AddCollab(myCollab, skillName);
 
-            //Output.ItemsSource = DataAccess.GetCollabList();
+            Output.ItemsSource = DataAccess.GetCollabList();
 
             collabNameInput.Text = "";
             collabResumeInput.Text = "";
             collabSkillInput.Text = "";
         }
+
+        private void AddProject(object sender, RoutedEventArgs e)
+        {
+            string[] collabName = ProjectCollabName.Text.Split(',');
+            Project myProject = new Project
+            {
+                Name = projectNameInput.Text,
+                Description = projectDescInput.Text,
+            };
+
+            DataAccess.AddProject(myProject, collabName);
+
+            Output.ItemsSource = DataAccess.GetCollabList();
+
+            projectNameInput.Text = "";
+            ProjectCollabName.Text = "";
+            projectDescInput.Text = "";
+
+        }
+
+        private void AddSkill(object sender, RoutedEventArgs e)
+        {
+            string[] collabName = skillCollabInput.Text.Split(',');
+            string[] projectName = skillProjectInput.Text.Split(',');
+            Skill mySkill = new Skill
+            {
+                Name = skillNameInput.Text,
+                Description = skillDescInput.Text
+            };
+
+            DataAccess.AddSkill(mySkill, collabName, projectName);
+
+            Output.ItemsSource = DataAccess.GetCollabList();
+
+            skillNameInput.Text = "";
+            skillDescInput.Text = "";
+            skillCollabInput.Text = "";
+            skillProjectInput.Text = "";
+
+        }
+
+        private void SuppCollab(object sender, RoutedEventArgs e)
+        {
+            DataAccess.SuppCollab(suppCollabInput.Text);
+
+            Output.ItemsSource = DataAccess.GetCollabList();
+
+            suppCollabInput.Text = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Name.Navigate(typeof(WordCloudPage));
+        }
+
     }
 }
+
